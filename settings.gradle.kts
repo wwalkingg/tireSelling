6cbb3b297e662ef0c4e8e56843145a295a388fda
wwalkingg <wwalkingg@outlook.com>
2023-04-14 20:28:56 +0800

    maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        maven("https://maven.aliyun.com/repository/public/")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
         maven("https://jitpack.io")
+        maven("https://maven.aliyun.com/repository/public/")
+        maven("https://maven.aliyun.com/repository/google")
     }
 }
 rootProject.name = "android"
